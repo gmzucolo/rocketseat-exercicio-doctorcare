45a781fe06ae28fb7fc48c91a522f7a5b0810bac
gmzucolo <gustavo.zucolo@hotmail.com>
2022-05-04 22:12:10 -0300

    dia03

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 function onScroll() {
   if (scrollY > 0) {
-    navigation.classList.add("scroll")
+    navigation.classList.add('scroll')
   } else {
-    navigation.classList.remove("scroll")
+    navigation.classList.remove('scroll')
   }
 }
 
 function openMenu() {
-  document.body.classList.add("expanded-menu")
+  document.body.classList.add('expanded-menu')
 }
 
 function closeMenu() {
-  document.body.classList.remove("expanded-menu")
+  document.body.classList.remove('expanded-menu')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  durantion: 300
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card,
+  #about,
+  #about header,
+  #about .content`)
